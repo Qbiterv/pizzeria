208c4d93b9fc6620c1a04d4bf682c920fba45e31
sebastian.kosmala <sebastiankosmala080@interia.pl>
2025-03-06 13:58:25 +0100

    added getAllIngredientsForMeal and fixes

--- meal/src/main/java/pl/auctane/meal/repositories/MealIngredientRepository.java
+++ meal/src/main/java/pl/auctane/meal/repositories/MealIngredientRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.auctane.meal.entities.MealIngredient;
 
+import java.util.List;
+
 @Repository
 public interface MealIngredientRepository extends JpaRepository<MealIngredient, Integer> {
+    List<MealIngredient> findAllByMealId_Id(int mealIdId);
 }
