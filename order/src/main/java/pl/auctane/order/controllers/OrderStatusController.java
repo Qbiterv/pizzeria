905d19ed69002e6f300dcb79e53259fe378b65a3
sebastian.kosmala <sebastiankosmala080@interia.pl>
2025-03-11 10:06:46 +0100

    added orderProduct classes

---
+++ order/src/main/java/pl/auctane/order/controllers/OrderStatusController.java
@@ -0,0 +1,4 @@
+package pl.auctane.order.controllers;
+
+public class OrderStatusController {
+}
--- order/src/main/java/pl/auctane/order/repositories/OrderProductRepository.java
+++ order/src/main/java/pl/auctane/order/repositories/OrderProductRepository.java
@@ -3,5 +3,8 @@ package pl.auctane.order.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.auctane.order.entities.OrderProduct;
 
+import java.util.List;
+
 public interface OrderProductRepository  extends JpaRepository<OrderProduct, Long> {
+    List<OrderProduct> findAllByOrder_Id(Long orderId);
 }
